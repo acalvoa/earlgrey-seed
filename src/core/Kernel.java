af2b12e2c4b21faa4cf8a6f945a66c574f67a9a0
Angelo Calvo A <angelo.calvoa@gmail.com>
2018-04-05 00:01:42 -0300

    Add the Earlgrey skeleton

---
+++ src/core/Kernel.java
@@ -0,0 +1,54 @@
+package test;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
+
+import earlgrey.core.Earlgrey;
+
+/**
+ * Application Lifecycle Listener implementation class SO
+ *
+ */
+@WebListener
+public class Kernel implements ServletContextListener, ServletRequestListener {
+
+    /**
+     * Default constructor. 
+     */
+    public Kernel() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletRequestListener#requestDestroyed(ServletRequestEvent)
+     */
+    public void requestDestroyed(ServletRequestEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletRequestListener#requestInitialized(ServletRequestEvent)
+     */
+    public void requestInitialized(ServletRequestEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub
+    	Earlgrey earlgrey = new Earlgrey(arg0.getServletContext());
+    }
+	
+}
